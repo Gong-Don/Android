a4bcc98eddd646bdb96e903d5f42cf51a9b164fb
leehandsub <gustjq7027@naver.com>
2022-02-16 23:32:31 +0900

    login+signup 액티비티 완성

---
+++ app/src/main/java/com/example/gong_don_android/LogInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gong_don_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
