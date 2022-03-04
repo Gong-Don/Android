809c19732de636f531da517dd548d248f3ff56cf
leehandsub <gustjq7027@naver.com>
2022-03-04 16:54:26 +0900

    정규식, 자동 로그인, 버튼 비활성화

--- app/src/main/java/com/example/gong_don_android/SignInActivity.kt
+++ app/src/main/java/com/example/gong_don_android/SignInActivity.kt
@@ -1,9 +1,9 @@
 package com.example.gong_don_android
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.gong_don_android.retrofit.ApiService
 import com.example.gong_don_android.retrofit.RetrofitClient
 import kotlinx.android.synthetic.main.activity_log_in.*
@@ -12,6 +12,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
+
 class SignInActivity : AppCompatActivity() {
     private lateinit var signinService : ApiService;
     private lateinit var retrofit : Retrofit
@@ -19,18 +20,19 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        val auto = getSharedPreferences("autoLogin", MODE_PRIVATE)
+        val autoLoginEdit = auto.edit()
+        var id = auto.getString("id", null)
+        var password = auto.getString("password", null)
 
         initRetrofit()
 
-        login_btn.setOnClickListener{
-            var id = input_id.text.toString()
-            var password = input_pw.text.toString()
+        if(id!=null&&password!=null)
+        {
             val data = SignIn(id, password)
-
             signinService.requestLogIn(data).enqueue(object : Callback<SignInResult> {
                 override fun onResponse(call: Call<SignInResult>, response: Response<SignInResult>) {
                     Log.d("SIGNIN RESULT", "${response.body()!!.userId}")
-
                 }
                 override fun onFailure(call: Call<SignInResult>, t: Throwable) {
                     Log.e("SIGNIN", t.message.toString())
@@ -40,6 +42,32 @@ class SignInActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+        else
+        {
+            login_btn.setOnClickListener{
+                id = input_id.text.toString()
+                password = input_pw.text.toString()
+                val data = SignIn(id, password)
+
+                signinService.requestLogIn(data).enqueue(object : Callback<SignInResult> {
+                    override fun onResponse(call: Call<SignInResult>, response: Response<SignInResult>) {
+                        Log.d("SIGNIN RESULT", "${response.body()!!.userId}")
+                        val auto = getSharedPreferences("autoLogin", MODE_PRIVATE)
+                        val autoLoginEdit = auto.edit()
+                        autoLoginEdit.putString("id", id)
+                        autoLoginEdit.putString("password", password)
+                        autoLoginEdit.apply()
+                    }
+                    override fun onFailure(call: Call<SignInResult>, t: Throwable) {
+                        Log.e("SIGNIN", t.message.toString())
+                    }
+                })
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
 
         signup_btn.setOnClickListener{
--- app/src/main/java/com/example/gong_don_android/SignUpActivity.kt
+++ app/src/main/java/com/example/gong_don_android/SignUpActivity.kt
@@ -1,9 +1,13 @@
 package com.example.gong_don_android
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import android.widget.Toast
 import com.example.gong_don_android.retrofit.ApiService
 import com.example.gong_don_android.retrofit.RetrofitClient
 import retrofit2.Call
@@ -14,7 +18,7 @@ import retrofit2.Retrofit
 class SignUpActivity : AppCompatActivity() {
     private lateinit var retrofitService : ApiService;
     private lateinit var retrofit : Retrofit
-
+    var mContext = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -25,39 +29,83 @@ class SignUpActivity : AppCompatActivity() {
 
         join_submit.setOnClickListener() {
             var name = input_nickname.text.toString()
+            //val isCheckname = namePattern(email)
             var password = input_pw.text.toString()
+            val isCheckpw = pwPattern(password)
             email = input_email.text.toString()
 
-            val data = SignUp(name, email, password, accessToken)
+            if(isCheckpw)
+            {
+                val data = SignUp(name, email, password, accessToken)
 
-            retrofitService.requestSignUp(data).enqueue(object : Callback<SignUpResult>{
-                override fun onResponse(call: Call<SignUpResult>, response: Response<SignUpResult>) {
-                    Log.d("SIGNUP RESULT", "${response.body()?.code}, ${response.body()?.message}")
-                }
-                override fun onFailure(call: Call<SignUpResult>, t: Throwable) {
-                    Log.e("SIGNUP", t.message.toString())
-                }
-            })
+                retrofitService.requestSignUp(data).enqueue(object : Callback<SignUpResult>{
+                    override fun onResponse(call: Call<SignUpResult>, response: Response<SignUpResult>) {
+                        Log.d("SIGNUP RESULT", "${response.body()?.code}, ${response.body()?.message}")
+                        Log.d("SIGNUP RESULT", accessToken)
+                        val intent = Intent(mContext, SignInActivity::class.java)
+                        startActivity(intent)
+                        Toast.makeText(mContext, "회원가입 성공", Toast.LENGTH_SHORT).show()// 회원가입성공메시지
+                        finish()
+                    }
+                    override fun onFailure(call: Call<SignUpResult>, t: Throwable) {
+                        Log.e("SIGNUP", t.message.toString())
+                    }
+                })
+            }
+            else
+            {
+                Toast.makeText(mContext, "비밀번호 형식이 맞지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         auth_btn.setOnClickListener() {
             accessToken = ""
             email = input_email.text.toString()
-            val data = Auth(email)
 
-            retrofitService.requestAuth(data).enqueue(object : Callback<AuthResult>{
-                override fun onResponse(call: Call<AuthResult>, response: Response<AuthResult>) {
-                    Log.d("AUTH RESULT", "${response.body()?.accessToken}")
-                    accessToken = response.body()!!.accessToken.toString()
-                }
-                override fun onFailure(call: Call<AuthResult>, t: Throwable) {
-                    Log.e("AUTH", t.message.toString())
-                }
-            })
+            val isCheckEmail = emailPattern(email)
+
+            if (isCheckEmail) {
+                val data = Auth(email)
+                retrofitService.requestAuth(data).enqueue(object : Callback<AuthResult>{
+                    override fun onResponse(call: Call<AuthResult>, response: Response<AuthResult>) {
+                        Log.d("AUTH RESULT", "${response.body()?.accessToken}")
+                        accessToken = response.body()!!.accessToken.toString()
+                    }
+                    override fun onFailure(call: Call<AuthResult>, t: Throwable) {
+                        Log.e("AUTH", t.message.toString())
+                    }
+                })
+                val countDown = object : CountDownTimer(1000 * 10, 1000) {
+                    override fun onTick(p0: Long) {
+                        // countDownInterval 마다 호출 (여기선 1000ms)
+                        auth_btn.isEnabled=false
+                    }
+
+                    override fun onFinish() {
+                        auth_btn.isEnabled=true
+                    }
+                }.start()//
+            }
+            else{
+                Toast.makeText(mContext, "이메일 형식이 맞지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
     private fun initRetrofit(){
         retrofit = RetrofitClient.create()
         retrofitService = retrofit.create(ApiService::class.java)
+    }
+    fun emailPattern(email: String): Boolean {//이메일
+        val repExp =
+            Regex("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+        return email.matches(repExp)
+    }
+    fun pwPattern(pw: String): Boolean {//비밀번호
+            val repExp = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,20}\$")
+        return pw.matches(repExp)
+    }
+    fun namePattern(pw: String): Boolean {//닉네임
+        val repExp = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,20}\$")
+        return pw.matches(repExp)
     }
 }
